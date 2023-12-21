2b02ce38f6b26e27ab988c5a4eb1d407f3686990
Maxime <gendron.maxime1@gmail.com>
2023-12-21 17:32:23 -0500

    chore: Removed useless stuff for this project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,14 @@ import { loadEnv } from 'vite';
 const { SITE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), '');
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  output: 'server',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     solidJs(),
-  ],
-  adapter: vercel()
+  ]
 });
--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -2,15 +2,11 @@ import type { LangKeys } from './config';
 
 export const ui = {
   fr: {
-    login: 'Se connecter',
-    logout: 'Se déconnecter',
     '404': '404 Page non trouvée',
     home: 'Accueil',
     index: 'Index',
   },
   en: {
-    login: 'Log in',
-    logout: 'Log out',
     '404': '404 Page not found',
     home: 'Home',
     index: 'Index',
